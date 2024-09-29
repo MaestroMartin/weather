918202a199d125cef2ea59f468785dcb85cd8c73
Martin Pelíšek <161080106+MaestroMartin@users.noreply.github.com>
2024-09-29 10:12:57 +0200

    Update weather.py
    remove api key

--- weather.py
+++ weather.py
@@ -7,7 +7,7 @@ city = "Brno "
 Base_URL = "https://api.openweathermap.org/data/2.5/weather?"
 City = "Brno"
 
-url = Base_URL + "appid=" + "ed780eb9027702dbd34f95f4c86b7cef" + "&q=" + City + "&units=metric"
+url = Base_URL + "appid=" + "api_key" + "&q=" + City + "&units=metric"
 
 response = requests.get(url).json()
 
